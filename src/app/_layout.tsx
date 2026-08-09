ea111a37921283aad8e6fcdcd83f25afb1c130f8
Mani <itsamanihd@gmail.com>
2026-08-09 23:29:53 +0100

    Layout Bug Fixes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,24 @@
 import { Slot } from "expo-router";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  // This state tracks if the website has actually loaded in the browser yet
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // If it is still building on the server, show nothing so it doesn't guess the wrong screen size!
+  if (!isMounted) return null;
+
   return (
-    <SafeAreaProvider style={{ flex: 1 }}>
-      <Slot />
+    <SafeAreaProvider>
+      <View style={{ flex: 1 }}>
+        <Slot />
+      </View>
     </SafeAreaProvider>
   );
 }
